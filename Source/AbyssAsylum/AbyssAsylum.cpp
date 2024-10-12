6858a7128f62def38e93494b11036b45979e2948
Bloin <Bolin8621@gmail.com>
2024-10-13 02:02:26 +0800

    Blank UE5 Project Initial Commit
    Blank UE5 Project Initial Commit.

---
+++ Source/AbyssAsylum/AbyssAsylum.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AbyssAsylum.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AbyssAsylum, "AbyssAsylum" );
